d26e58b414ec45340dc8483e00f795a3a9382d3e
Avinash <36784277+Avi-1996@users.noreply.github.com>
2019-02-01 15:19:14 +0530

    Create mylistMainActivity.java

---
+++ Android Studio Code/CustomLayout Using Base Adapter/mylistMainActivity.java
@@ -0,0 +1,31 @@
+package edu.kiet.www.mylistviewexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    String lagData[] = {"C","C++","Android Programin","Python","C","C++","Android Programin","Python"};
+    ArrayAdapter<String> adapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listView = findViewById(R.id.listView);
+        adapter = new ArrayAdapter<String>(getApplicationContext(),R.layout.mydatalist,R.id.textView2,lagData);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(getApplicationContext(), adapter.getItem(i), Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+}
